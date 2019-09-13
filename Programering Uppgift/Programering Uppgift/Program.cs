4dfc99b2ee056fec6b397f498a639d6c0df0a115
nahoJW <54435179+nahoJW@users.noreply.github.com>
2019-09-13 09:48:50 +0200

    + Comments

--- Programering Uppgift/Programering Uppgift/Program.cs
+++ Programering Uppgift/Programering Uppgift/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Programering_Uppgift
 {
@@ -6,36 +6,37 @@ namespace Programering_Uppgift
     {
         static void Main(string[] args)
         {
-            Console.Write("Skriv ett tal mellan 1 och 100: "); //Skriver ut text
-            Random rnd = new Random();//Lägger till 
-            int RandomN = rnd.Next(0, 100);
-            int AntalF = 1;
+            Console.Write("Skriv ett tal mellan 1 och 100: "); //Writes text 
+            Random rnd = new Random(); 
+            int RandomN = rnd.Next(0, 100);//Choose a number between 1 and 100
+            int AntalF = 1; //Antal försök
         while (true)
             {
-                int WNumber = int.Parse(Console.ReadLine());
+                int WNumber = int.Parse(Console.ReadLine());//Creates variable for the writen number
 
-                AntalF++;
+                AntalF++;//+1 on number of tries
 
                 if (RandomN == WNumber)
                 { 
                      Console.WriteLine("Du vann");
                     Console.WriteLine("Antal försök: "+""+AntalF);
                      break;
-                }
+                }//If you're right write "you won", number of tries and ends the game
 
                 if (RandomN < WNumber)
                 {
                     Console.WriteLine("Du gissade för högt");
                     Console.WriteLine("Gissa igen");
-                }
+                }//If to hight type "to high" and "try again"
 
                 if (RandomN > WNumber)
                 {
                     Console.WriteLine("Du gissade för lågt");
                     Console.WriteLine("Gissa igen");
-                }
+                }//If to low type "to low" and "try again"
             }
 
         }
     }
 }
+
