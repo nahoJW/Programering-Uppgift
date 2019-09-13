859084d8f5011a85f6a3ca6621cf0039637b511e
Elev <Elev@TEB123009>
2019-09-13 09:31:56 +0200

    + Antal försök

--- Programering Uppgift/Programering Uppgift/Program.cs
+++ Programering Uppgift/Programering Uppgift/Program.cs
@@ -6,16 +6,20 @@ namespace Programering_Uppgift
     {
         static void Main(string[] args)
         {
-            Console.Write("Skriv ett tal: ");
-            Random rnd = new Random();
+            Console.Write("Skriv ett tal mellan 1 och 100: "); //Skriver ut text
+            Random rnd = new Random();//Lägger till 
             int RandomN = rnd.Next(0, 100);
+            int AntalF = 1;
         while (true)
             {
                 int WNumber = int.Parse(Console.ReadLine());
 
+                AntalF++;
+
                 if (RandomN == WNumber)
                 { 
                      Console.WriteLine("Du vann");
+                    Console.WriteLine("Antal försök: "+""+AntalF);
                      break;
                 }
 
