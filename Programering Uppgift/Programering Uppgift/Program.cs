661c1942a888a84f99f533f29e5dd64d54a5caad
Elev <Elev@TEB123009>
2019-09-13 08:37:42 +0200

    added random generated int number

---
+++ Programering Uppgift/Programering Uppgift/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Programering_Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv ett tal: ");
+            Random rnd = new Random();
+            int Number = rnd.Next(0, 100);
+        
+        }
+    }
+}
