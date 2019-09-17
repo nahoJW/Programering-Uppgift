c79af397b30ac356b7cf68e38a7153d806b62f95
nahoJW <54435179+nahoJW@users.noreply.github.com>
2019-09-17 12:39:47 +0200

    Uppdaterade kommentarer

--- Programering Uppgift/Programering Uppgift/Program.cs
+++ Programering Uppgift/Programering Uppgift/Program.cs
@@ -6,34 +6,34 @@ namespace Programering_Uppgift
     {
         static void Main(string[] args)
         {
-            Console.Write("Skriv ett tal mellan 1 och 100: "); //Writes text 
-            Random rnd = new Random(); 
-            int RandomN = rnd.Next(0, 100);//Choose a number between 1 and 100
-            int AntalF = 1; //Antal försök
-        while (true)
+            Console.Write("Skriv ett tal mellan 1 och 100: "); //Skriver in text 
+            Random rnd = new Random(); //Generarear ett slumpmässig tal
+            int RandomN = rnd.Next(0, 100);//Sägger att talet ska vara mellan 0 och 100
+            int AntalF = 1; //Skapar en variabel för antal försök
+        while (true) //Skapar en loop för att man ska kunna gissa flera gånger
             {
-                int WNumber = int.Parse(Console.ReadLine());//Creates variable for the writen number
+                int WNumber = int.Parse(Console.ReadLine());//Spakar en variabel för numret som skrivits in
 
-                AntalF++;//+1 on number of tries
+                AntalF++;//Lägger till 1 på antal försök så att det ökar med en vaje gång loopen upprepas
 
-                if (RandomN == WNumber)
+                if (RandomN == WNumber) //Ifall det slupmässiga talet är samma som det inskrivna så händer det inom: {}
                 { 
-                     Console.WriteLine("Du vann");
-                    Console.WriteLine("Antal försök: "+""+AntalF);
-                     break;
-                }//If you're right write "you won", number of tries and ends the game
+                     Console.WriteLine("Du vann");//Skriver att personen vann 
+                    Console.WriteLine("Antal försök: "+""+AntalF);//Skriver ut hur många gånger personen försökt
+                     break;//stoppar loopen
+                }
 
-                if (RandomN < WNumber)
+                if (RandomN < WNumber)//Ifall det slupmässiga<inskrivna så händer det inom: {}
                 {
-                    Console.WriteLine("Du gissade för högt");
-                    Console.WriteLine("Gissa igen");
-                }//If to hight type "to high" and "try again"
+                    Console.WriteLine("Du gissade för högt");//Skirver att personen gissa för högt
+                    Console.WriteLine("Gissa igen");//Skriver gissa igen
+                }
 
-                if (RandomN > WNumber)
+                if (RandomN > WNumber) //Ifall det slupmässiga talet > det inskrivna talet så händer det inom: {}
                 {
-                    Console.WriteLine("Du gissade för lågt");
-                    Console.WriteLine("Gissa igen");
-                }//If to low type "to low" and "try again"
+                    Console.WriteLine("Du gissade för lågt");//Skriver att personen gissade för högt
+                    Console.WriteLine("Gissa igen");//Skriver att personen ska försöka igen
+                }
             }
 
         }
